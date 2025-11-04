192e523f253d9419a11be282cfdf95eb42efbc08
Maksym Prokopov <maksym.prokopov@billie.io>
2025-11-04 10:43:49 +0100

    install ruby-lsp and drop solargraph

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,7 +2,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local nvlsp = require "nvchad.configs.lspconfig"
-local servers = { "html", "cssls", "solargraph", "terraformls", "rubocop", "eslint", "ansiblels", "ts_ls", "helm_ls", "yamlls", "clojure_lsp", "nushell" }
+local servers = { "html", "cssls", "terraformls", "eslint", "ansiblels", "ts_ls", "helm_ls", "yamlls", "clojure_lsp", "nushell", "ruby_lsp" }
 
 -- Configure and enable LSP servers using vim.lsp.config (Neovim 0.11+)
 for _, lsp in ipairs(servers) do
@@ -13,6 +13,16 @@ for _, lsp in ipairs(servers) do
   }
   vim.lsp.enable(lsp)
 end
+
+-- Configure rubocop separately to use system rubocop with rbenv
+vim.lsp.config.rubocop = {
+  cmd = { vim.fn.expand("~/.rbenv/shims/rubocop"), "--lsp" },
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  root_dir = vim.fs.root(0, { ".rubocop.yml", "Gemfile" }),
+}
+vim.lsp.enable("rubocop")
 
 -- typescript
 -- lspconfig.tsserver.setup {
